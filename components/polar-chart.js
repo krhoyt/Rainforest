923b8f10d82938e6a5021fcde4eeab53e84103f3
Kevin Hoyt <parkerkrhoyt@gmail.com>
2023-11-09 22:25:12 -0700

    Cleaning up rendering
    Optimizing where I can, enhancing if possible.

--- components/polar-chart.js
+++ components/polar-chart.js
@@ -10,8 +10,19 @@ export default class RainforestPolarChart extends HTMLElement {
           display: inline-block;
           position: relative;
         }
+
+        g[part=series] circle {
+          transition:
+            fill 0.60s,
+            opacity 0.60s
+        }
       </style>
-      <svg></svg>
+      <svg part="vector">
+        <g part="chart">
+          <g part="background"></g>
+          <g part="series"></g>
+        </g>
+      </svg>
     `;
 
     // Private
@@ -32,26 +43,150 @@ export default class RainforestPolarChart extends HTMLElement {
     ];
     this._series = [];
 
+    // Events
+    this.onPointOver = this.onPointOver.bind( this );
+    this.onPointOut = this.onPointOut.bind( this );        
+    this.onSeriesOver = this.onSeriesOver.bind( this );
+    this.onSeriesOut = this.onSeriesOut.bind( this );    
+
     // Root
     this.attachShadow( {mode: 'open'} );
     this.shadowRoot.appendChild( template.content.cloneNode( true ) );
 
     // Elements
-    this.$vector = this.shadowRoot.querySelector( 'svg' );
+    this.$background = this.shadowRoot.querySelector( 'g[part=background]' );
+    this.$chart = this.shadowRoot.querySelector( 'g[part=chart]' );
+    this.$series = this.shadowRoot.querySelector( 'g[part=series]' );
+    this.$vector = this.shadowRoot.querySelector( 'svg' );    
   }
 
-  _spokes( count, size ) {
+  onPointOut( evt ) {
+    evt.currentTarget.setAttributeNS( null, 'fill', '#ffffff' );
+  }
+
+  onPointOver( evt ) {
+    console.log( 'Value: ' + evt.currentTarget.getAttribute( 'data-value' ) );
+    console.log( 'Angle: ' + evt.currentTarget.getAttribute( 'data-angle' ) );
+    console.log( 'Index: ' + evt.currentTarget.getAttribute( 'data-index' ) );  
+    
+    evt.currentTarget.setAttributeNS( null, 'fill', evt.currentTarget.getAttribute( 'stroke' ) );
+  }
+
+  onSeriesOut( evt ) {
+    for( let c = 0; c < evt.currentTarget.children.length; c++ ) {
+      if( evt.currentTarget.children[c].tagName === 'circle' ) {
+        evt.currentTarget.children[c].style.opacity = '0';
+      }
+    }
+  }
+
+  onSeriesOver( evt ) {
+    const index = parseInt( evt.currentTarget.getAttribute( 'data-series' ) );
+    console.log( 'Series: ' + this._series[index].title );
+
+    for( let c = 0; c < evt.currentTarget.children.length; c++ ) {
+      if( evt.currentTarget.children[c].tagName === 'circle' ) {
+        evt.currentTarget.children[c].style.opacity = '1.0';
+      }
+    }
+  }
+
+  _area( radius, count, minimum, maximum, data, color ) {
+    const slice = ( 360 / count ) * ( Math.PI / 180 );
+
     const group = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
+    group.style.cursor = 'pointer';
+    group.addEventListener( 'mouseover', this.onSeriesOver );
+    group.addEventListener( 'mouseout', this.onSeriesOut );   
+
+    const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
+    path.setAttributeNS( null, 'fill', `${color}66` );
+    path.setAttributeNS( null, 'stroke', color );
+    path.setAttributeNS( null, 'stroke-width', 2 );
+    path.setAttributeNS( null, 'stroke-linecap', 'round' );
+    group.appendChild( path );
+
+    let d = '';
+
+    for( let c = 0; c < data.length; c++ ) {
+      const mapped = this._map( data[c], minimum, maximum, 0, radius );
+      const x = mapped * Math.sin( slice * c );
+      const y = mapped * ( 0 - Math.cos( slice * c ) );
+
+      if( c === 0 ) {
+        d = `M ${x} ${y} `;
+      } else {
+        d = d + `L ${x} ${y} `;
+      }
+
+      const circle = document.createElementNS( 'http://www.w3.org/2000/svg', 'circle' );
+      circle.addEventListener( 'mouseover', this.onPointOver );
+      circle.addEventListener( 'mouseout', this.onPointOut );      
+      circle.setAttributeNS( null, 'fill', '#ffffff' );
+      circle.setAttributeNS( null, 'stroke', color );
+      circle.setAttributeNS( null, 'stroke-width', 2 );    
+      circle.setAttributeNS( null, 'r', 4 );    
+      circle.setAttributeNS( null, 'cx', x );    
+      circle.setAttributeNS( null, 'cy', y );       
+      circle.setAttribute( 'data-value', data[c] );
+      circle.setAttribute( 'data-angle', ( 360 / count ) * c );
+      circle.setAttribute( 'data-index', c );
+      circle.style.opacity = '0';   
+      group.appendChild( circle );  
+    }
+
+    path.setAttributeNS( null, 'd', d + 'Z' );
+    return group;
+  }
+
+  _map( x, in_min, in_max, out_min, out_max ) {
+    return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+  }  
+
+  _polygons( count, radius, stops ) {
+    const group = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
+    const slice = ( 360 / count ) * ( Math.PI / 180 );
+    const levels = radius / stops;    
+
+    for( let p = 0; p < 5; p++ ) {
+      const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
+      path.setAttributeNS( null, 'fill', 'none' );
+      path.setAttributeNS( null, 'stroke', '#d1d5db' );
+
+      let d = '';
+
+      for( let c = 0; c < count; c++ ) {
+        const x = ( radius - ( levels * p ) ) * Math.sin( slice * c );
+        const y = ( radius - ( levels * p ) ) * ( 0 - Math.cos( slice * c ) );
+
+        if( c === 0 ) {
+          d = `M ${x} ${y} `;
+        } else {
+          d = d + `L ${x} ${y} `;
+        }
+      }
+
+      path.setAttributeNS( null, 'd', d + 'Z' );         
+      group.appendChild( path );
+    }
+
+    return group;
+  }  
+
+  _spokes( count, radius ) {
+    const group = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
+
     for( let c = 0; c < count; c++ ) {
       const line = document.createElementNS( 'http://www.w3.org/2000/svg', 'rect' );
       line.setAttributeNS( null, 'fill', '#d1d5db' );
       line.setAttributeNS( null, 'width', 1 );
-      line.setAttributeNS( null, 'height', size / 2 );        
+      line.setAttributeNS( null, 'height', radius );        
       line.setAttributeNS( null, 'x', -0.50 );
-      line.setAttributeNS( null, 'y', 0 - ( size / 2 ) );
+      line.setAttributeNS( null, 'y', 0 - radius );
       line.setAttributeNS( null, 'transform', `rotate( ${( 360 / count ) * c} )` );
       group.appendChild( line );
     }
+
     return group;
   }  
 
@@ -71,6 +206,7 @@ export default class RainforestPolarChart extends HTMLElement {
   // Setup
   connectedCallback() {
     this._upgrade( 'series' );                               
+    this._upgrade( 'domain' );                                   
     this._render();
   }
 
@@ -106,117 +242,22 @@ export default class RainforestPolarChart extends HTMLElement {
         return inner < value ? inner : value;
       }, maximum );
       const size = Math.min( this.$vector.clientWidth, this.$vector.clientHeight );
+      const radius = size / 2;
 
-      const chart = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
-      const spokes = this._spokes( count, size );
-      const polygons = this._polygons( count, size );
-      chart.appendChild( spokes );
-      chart.appendChild( polygons );      
+      const spokes = this._spokes( count, radius );
+      this.$background.appendChild( spokes );      
+
+      const polygons = this._polygons( count, radius, 5 );
+      this.$background.appendChild( polygons );      
+
       for( let s = 0; s < this._series.length; s++ ) {
-        const area = this._area( size, count, 0, 5, this._series[s].data, this._colors[s] );
+        const area = this._area( radius, count, 0, 5, this._series[s].data, this._colors[s] );
         area.setAttribute( 'data-series', s );
-        chart.appendChild( area );
+        this.$series.appendChild( area );
       }
-      chart.setAttributeNS( null, 'transform', `translate( ${size / 2} ${size / 2} )` );
-      this.$vector.appendChild( chart );
+
+      this.$chart.setAttributeNS( null, 'transform', `translate( ${this.$vector.clientWidth / 2} ${this.$vector.clientHeight / 2} )` );
     }
-  }
-
-  _map( x, in_min, in_max, out_min, out_max ) {
-    return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
-  }
-
-  _area( size, count, minimum, maximum, data, color ) {
-    const slice = ( 360 / count ) * ( Math.PI / 180 );
-
-    const group = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
-    group.style.cursor = 'pointer';
-    group.addEventListener( 'mouseover', ( evt ) => {
-      for( let c = 0; c < evt.currentTarget.children.length; c++ ) {
-        if( evt.currentTarget.children[c].tagName === 'circle' ) {
-          evt.currentTarget.children[c].style.opacity = '1.0';
-        }
-      }
-    } );
-    group.addEventListener( 'mouseout', ( evt ) => {
-      for( let c = 0; c < evt.currentTarget.children.length; c++ ) {
-        if( evt.currentTarget.children[c].tagName === 'circle' ) {
-          evt.currentTarget.children[c].style.opacity = '0';
-        }
-      }
-    } );    
-    const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
-    path.setAttributeNS( null, 'fill', `${color}66` );
-    path.setAttributeNS( null, 'stroke', color );
-    path.setAttributeNS( null, 'stroke-width', 2 );
-    path.setAttributeNS( null, 'stroke-linecap', 'round' );
-    group.appendChild( path );
-
-    let d = '';
-
-    for( let c = 0; c < data.length; c++ ) {
-      const mapped = this._map( data[c], minimum, maximum, 0, size / 2 );
-      const x = mapped * Math.sin( slice * c );
-      const y = mapped * ( 0 - Math.cos( slice * c ) );
-
-      if( c === 0 ) {
-        d = `M ${x} ${y} `;
-      } else {
-        d = d + `L ${x} ${y} `;
-      }
-
-      const circle = document.createElementNS( 'http://www.w3.org/2000/svg', 'circle' );
-      circle.addEventListener( 'mouseover', ( evt ) => {
-        console.log( evt.currentTarget.getAttribute( 'data-value' ) );
-        console.log( evt.currentTarget.getAttribute( 'data-angle' ) );
-        console.log( evt.currentTarget.getAttribute( 'data-index' ) );                
-      } );
-      circle.setAttributeNS( null, 'fill', '#ffffff' );
-      circle.setAttributeNS( null, 'stroke', color );
-      circle.setAttributeNS( null, 'stroke-width', 2 );    
-      circle.setAttributeNS( null, 'r', 4 );    
-      circle.setAttributeNS( null, 'cx', x );    
-      circle.setAttributeNS( null, 'cy', y );       
-      circle.setAttribute( 'data-value', data[c] );
-      circle.setAttribute( 'data-angle', ( 360 / count ) * c );
-      circle.setAttribute( 'data-index', c );
-      circle.style.opacity = '0';   
-      group.appendChild( circle );  
-    }
-
-    path.setAttributeNS( null, 'd', d + 'Z' );
-    return group;
-  }
-
-  _polygons( count, size ) {
-    const group = document.createElementNS( 'http://www.w3.org/2000/svg', 'g' );
-    const slice = ( 360 / count ) * ( Math.PI / 180 );
-    const levels = ( size / 2 ) / 5;    
-
-    for( let p = 0; p < 5; p++ ) {
-      const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
-
-      path.setAttributeNS( null, 'fill', 'none' );
-      path.setAttributeNS( null, 'stroke', '#d1d5db' );
-
-      let d = '';
-
-      for( let c = 0; c < count; c++ ) {
-        const x = ( ( size / 2 ) - ( levels * p ) ) * Math.sin( slice * c );
-        const y = ( ( size / 2 ) - ( levels * p ) ) * ( 0 - Math.cos( slice * c ) );
-
-        if( c === 0 ) {
-          d = `M ${x} ${y} `;
-        } else {
-          d = d + `L ${x} ${y} `;
-        }
-      }
-
-      path.setAttributeNS( null, 'd', d + 'Z' );         
-      group.appendChild( path );
-    }
-
-    return group;
   }
 
   // Attributes
